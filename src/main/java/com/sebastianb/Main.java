1f932d77a50c2f4b5e4e1dd1e1ac832e6b8badaa
Sebastian S <74125017+sebasinmas@users.noreply.github.com>
2025-06-23 21:36:58 -0400

    first commit

---
+++ src/main/java/com/sebastianb/Main.java
@@ -0,0 +1,7 @@
+package com.sebastianb;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/test/java/MkCharacterTest.java
@@ -0,0 +1,29 @@
+import org.junit.jupiter.api.Test;
+
+public class MkCharacterTest {
+    // Esta clase define los tests para una api crud de personajes de Mortal Kombat.
+
+    
+    @Test
+    public void testCreateCharacter(){
+        
+        assert();
+    }
+    
+    @Test
+    public void testUpdateCharacter() {
+        // Test cases for character-related functionality
+    
+    }
+
+    @Test
+    public void testDeleteCharacter() {
+        // Test cases for character-related functionality
+    }
+
+    @Test
+    public void testGetCharacter() {
+        // Test cases for character-related functionality
+    }
+
+}
